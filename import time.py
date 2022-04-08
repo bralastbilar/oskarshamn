98d25ccc00d1f4885e6345d54a5833cb17f8b760
bralastbilar <103260361+bralastbilar@users.noreply.github.com>
2022-04-08 14:48:55 +0200

    Add files via upload
    AutomationGruops vardagskod.

---
+++ import time.py
@@ -0,0 +1,23 @@
+import time
+while True:
+
+
+    print("Hallå mupp!  ")
+    time.sleep(1)
+    name = input('vad heter du?')
+    time.sleep(1)
+    print('hej hej! '+name)
+    time.sleep(1)
+    answer = input('vet du vad jag hört...?')
+    if answer == ('nej'):#indenteringen styr kodblocket!!!
+        time.sleep(2)
+        print('ALLT ÄR FAN SKIT!')
+        time.sleep(1)
+        print('PRECIS ALLT!!!')
+        time.sleep(1)
+        print('Alltihop...')
+    elif not answer == ('nej'):
+        print('Vi som vet vi vet...')
+        time.sleep(2)
+        print('Du vet.')
+        time.sleep(5)
